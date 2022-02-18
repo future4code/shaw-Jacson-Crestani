20b1518d584918699a60cff5c357145be9bc89cb
Jacson Crestani <jcrestanij@gmail.com>
2022-02-17 23:02:45 -0300

    Exercício de Funções

---
+++ modulo1/funções/index.js
@@ -0,0 +1,130 @@
+//Exercício 1
+
+//a)
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))        //  O valor de 5 * 2 === 10
+console.log(minhaFuncao(10))*/      //  O valor de 5 * 10 === 50
+
+
+//b) 
+
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5            
+}
+
+minhaFuncao(2)         // não mostra nada, por nao ter o console.log 
+minhaFuncao(10)*/
+
+
+//Exrcício 2
+
+//a)
+
+/*let textoDoUsuario = prompt("Insira um texto"); // Pede ao usuario uma frase, que retornara tudo minuscula e se tiver cenoura true se não false
+
+const outraFuncao = function(texto) { 
+	return texto.toLowerCase().includes("cenoura")                                                             
+}                                                      
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+
+// i: minuscula,true, undefined
+// ii: minuscula,true, undefined
+// iii: minuscula, true, undefined
+
+
+//Exercício Escrita de código
+
+//a)
+
+
+/*function mostraTela(){
+   console.log(`Eu sou Jacson, tenho 32 anos, moro em Santa Catarina e sou estudante`)
+
+}
+
+mostraTela()*/
+
+
+//b)
+
+/*function mostrar(nome,idade,cidade,profissao){
+   console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade}, e sou ${profissao}`)
+}
+
+mostrar("Jacson",32,"Jpinville","estudante")*/
+
+
+//Exercício 2
+
+
+//a)
+
+/*const a = 5
+const b = 10
+
+function somar(a, b){
+    return a + b
+}
+const mostrarNaTela = somar(5,10)
+console.log(somar(5,10))*/
+
+
+//b)
+
+
+/*function comparar(n1,n2){
+   return n1 >= n2
+}
+
+console.log(comparar(a,b))*/
+
+
+//c)
+
+/*const a = 10  //true
+const b = 15    //false
+
+function comparar(n1){
+   return n1 % 2 === 0
+}
+console.log(comparar(a))
+console.log(comparar(b))*/
+
+
+/*let variavel = "Bom dia........vamo que vamo!!!!!!!"
+variavel =variavel.toUpperCase().trim().replaceAll(" ","-")
+console.log(variavel)*/
+
+
+//Exercício 3
+
+/*function soma(n1,n2){
+   return n1 / n2
+}
+
+function subtracao(n1,n2){
+   return n1 / n2
+}
+
+function multiplicacao(n1,n2){
+   return n1 / n2
+}
+
+function divisao(n1,n2){
+   return n1 / n2
+}
+
+const numero1 = Number(prompt("Digite um numero:"))
+const numero2 = Number(prompt("Digite outro numero:"))
+
+console.log(soma(numero1, numero2))
+console.log(subtracao(numero1, numero2))
+console.log(multiplicacao(numero1, numero2))
+console.log(divisao(numero1, numero2))*/
