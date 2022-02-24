4ef0fee73a2a20109575adae394433bc13e66af2
jcrestani <98922855+jcrestanij@users.noreply.github.com>
2022-02-24 16:49:37 -0300

    Update index.js

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -45,9 +45,9 @@ let a = 10
  let b = 25
   let c = 25
 
-  a = c  // a recebe 25
+ a = c  // a recebe 25
   b = a  // b recebe 10
-  c = b // c recebe 25
+   c = b // c recebe 25
   
-  console.log("O novo valor de a é", a) //valor de a é 25
-  console.log("O novo valor de b é", b) // valor de b é 10
+ console.log("O novo valor de a é", a) //valor de a é 25
+ console.log("O novo valor de b é", b) // valor de b é 10
