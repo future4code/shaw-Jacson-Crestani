b4d44a65af1450c0e70d073ce983f4c41b8cca76
jcrestani <98922855+jcrestanij@users.noreply.github.com>
2022-02-25 08:19:38 -0300

    Update index.js

--- modulo1/laços/index.js
+++ modulo1/laços/index.js
@@ -1,75 +1,63 @@
 //Exercícios de interpretação de código 1
 
-/*
+
 let valor = 0                
 for(let i = 0; i < 5; i++) {
-  valor += i
+valor += i
 }
-console.log(valor)
-*/
+	console.log(valor);
+
 /* valor inicial 0, a cada passegem pelo loop ele 
-encrementa mais 1 */
+incrementa mais 1 */
 
 
 //Exercícios de interpretação de código 2
 
 
-/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
 for (let numero of lista) {
-  if (numero > 18) {
-		console.log(numero)
+if (numero > 18) {
+	console.log(numero);
 	}
-}*/
+}
 
 /* todo valor maior que 18 ele imprime no console, se o
 valor for menor, volta sem imprimir no console*/
 
 
-/*
-const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "));
 let quantidadeAtual = 0
 while(quantidadeAtual < quantidadeTotal){
-  let linha = ""
-  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+let linha = ""
+for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
     linha += "*"
   }
-  console.log(linha)
-  quantidadeAtual++
-}*/
+	console.log(linha);
+     quantidadeAtual++
+}
 
-/* sera imprimido a cada linha, * + 1 a cada linha */
-
-
-
-
-
-
-
-
-
-
-
-
+//sera imprimido a cada linha, * + 1 a cada linha 
 
 
 
 //Exercícios de escrita de código 1
 
-/*
-let bichinhos = Number(prompt("Quantos bichinhos você tem?"))
-    if (bichinhos <= 0) {
-    console.log("Que pena! Você pode adotar um pet!")
 
-    }else { 
+let bichinhos = Number(prompt("Quantos bichinhos você tem?"));
+if (bichinhos <= 0) {
+         console.log("Que pena! Você pode adotar um pet!");
+
+}else { 
         array = []
         for (let i = 0; i < bichinhos; i++) {
-            let nomebichinho = prompt("Digite o nome dos bichinhos: ")
-            array.push(nomebichinho)
-        }
-        
-        console.log(array)
+            let nomebichinho = prompt("Digite o nome dos bichinhos: ");
+            array.push(nomebichinho);
 }
-*/
+        
+	 console.log(array);
+}
+
 
 
 //Exercícios de escrita de código 2
@@ -77,16 +65,16 @@ let bichinhos = Number(prompt("Quantos bichinhos você tem?"))
 
 //a
 function funcao1 (array) {
-    for (let i = 0; i < array.length; i++)
-    const element = array[1]
+for (let i = 0; i < array.length; i++);
+const element = array[1]
 }
 
 
 
 //b
 function funcao(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]/10);
+for (let i = 0; i < array.length; i++) {
+         console.log(array[i]/10);
     }    
 }
 
@@ -94,27 +82,17 @@ function funcao(array) {
 //c
 
 function funcao (array) {
-    let array2 = []
+let array2 = []
 }
-
-
-
-
-
-
-
-
-
-
 
 
 //d
 function name(array) {
     let array1 = []
         for (let i = 0; i < array.length; i++) {
-        array.push (`O elemento do index ${i} é ${array1}`)
+        array.push (`O elemento do index ${i} é ${array1}`);
     }
-        console.log (array1)
+        console.log (array1);
 }
 
 
