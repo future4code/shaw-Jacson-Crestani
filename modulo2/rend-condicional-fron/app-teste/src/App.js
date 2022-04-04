7c9510a32fc480c8eaca498fb99ed9dc48e34ee5
jcrestani <jcrestanij@gmail.com>
2022-04-04 15:37:36 -0300

    PlayList

---
+++ modulo2/rend-condicional-fron/app-teste/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+export default class App extends React.Component {
+  state = {
+    nome: '',
+    idade: '',
+    Email: '',
+    QualOGrauDeEscolaridade: 
+  }
+}<div id="root">
+<div class="sc-gzVnrw cNNHdB">
+  <div class="sc-htoDjs bicDbn">
+    </div>
+    <form class="sc-bdVaJa eJlHkt">
+      <input type="text"
+       placeholder="Usuário"
+        class="sc-bwzfXH eAHCAl"
+         value="" spellcheck="false"
+    data-ms-editor="  true">
+            <input type="text"f
+             placeholder="Mensagem"
+              class="sc-htpNat kExHMy"
+               value="" spellcheck="false"
+                data-ms-editor="true">
+                  <button class="sc-bxivhb hiyNhT">Enviar</button>
+                  </form>
+                  </div>
+                  </div>
