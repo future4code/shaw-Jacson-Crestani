b102802a7967facd67177c207cdeac21ab427d3b
jcrestani <jcrestanij@gmail.com>
2022-04-19 16:45:13 -0300

    aula 32 use-effect

--- modulo3/aula32-use-effect/aula32-use-effect/src/App.js
+++ modulo3/aula32-use-effect/aula32-use-effect/src/App.js
@@ -1,57 +1,50 @@
-import React from "react";
-import "./styles.css";
+import { React, useState, useEffect } from "react";
 import axios from "axios";
 import PokeCard from "./components/PokeCard/PokeCard";
+import styled from "styled-components"
 
-class App extends React.Component {
-  state = {
-    // lista de pokemons que está sendo guardada no estado
-    pokeList: [],
-    // nome do pokemon guardado no estado, assim que o usuário
-    // escolhe um nome no dropdown
-    pokeName: ""
-  };
+const DivApp = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+  background-color: greenyellow;
+`
 
-  // método que roda após a montagem do componente
-  componentDidMount = () => {
-    // função axios que está batendo na API e buscando 151 pokemons
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+function App() {
+  const [pokeList, SetpokeList] = useState([])
+  const [pokeName, SetpokeName] = useState("")
+
+
+  const getPokemons = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
       .then(response => {
-        // função que está setando no estado os 151 pokemons
-        this.setState({ pokeList: response.data.results });
+        SetpokeList(response.data.results)
       })
       .catch(err => {
-        console.log(err);
-      });
-  };
-
-  changePokeName = event => {
-    this.setState({ pokeName: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        {/* evento onChange chama função toda vez que o usuário 
-        escolhe um novo pokemon no dropdown */}
-        <select onChange={this.changePokeName}>
-          <option value={""}>Nenhum</option>
-          {/* renderizando a lista de pokemons como opções do select */}
-          {this.state.pokeList.map(pokemon => {
-            return (
-              <option key={pokemon.name} value={pokemon.name}>
-                {pokemon.name}
-              </option>
-            );
-          })}
-        </select>
-        {/* expressão booleana que renderiza o componente PokeCard,
-        caso o valor de pokeName, no estado, seja true */}
-        {this.state.pokeName && <PokeCard pokemon={this.state.pokeName} />}
-      </div>
-    );
+        console.log(err)
+      })
   }
+  useEffect(() => {
+    getPokemons()
+  }, [])
+
+  const changePokename = event => {
+    SetpokeName(event.target.value)
+  }
+
+  return (
+    <DivApp>
+      <select onChange={changePokename}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>);
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
+    </DivApp>
+  );
 }
 
 export default App;
