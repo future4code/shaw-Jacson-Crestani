4c836139c782411c9d45ea84d14819228f7b51dd
jcrestani <jcrestanij@gmail.com>
2022-05-02 08:13:39 -0300

    paginas

--- modulo3/labex/labex-app/src/App.js
+++ modulo3/labex/labex-app/src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
-import {Router}  from "./pages/routes/Router";
+import axios from "axios";
+import React, { useState } from "react";
 
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
- const App = () => {
+  const mudaEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const mudaSenha = (event) => {
+    setSenha(event.target.value);
+  };
+
+  const fazerLogin = () => {
+    event.preventDefault();
+
+    const body = {
+      email: email,
+      password: senha
+    };
+
+    console.log(body)
+    //axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/jacson-crestani-shaw/login", body, headers")
+  };
 
   return (
     <div>
-      <Router/>
+      <h1>Login</h1>
+      <form onSubmit={fazerLogin}>
+        <input value={email} onChange={mudaEmail} placeholder={"E-mail"} />
+        <input
+          type="password"
+          value={senha}
+          onChange={mudaSenha}
+          placeholder={"Senha"}
+        />
+        <button>Fazer Login</button>
+      </form>
     </div>
   );
 };
 
 export default App;
- 
